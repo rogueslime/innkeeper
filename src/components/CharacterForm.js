2b22b2c852eeec80c2cf3c553cdc7f911fa7b4d4
Berry <trayton-e-berry@utc.edu>
2024-02-12 14:57:16 -0500

    Began expanding front-end CharacterForm component to take in all of the required character information. Subforms inside the main form will be used to build arrays of Ascores, feats, spells, and items that are submitted in the main form and should be displayed above each subform.

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -1,25 +1,57 @@
 import React, {useState} from 'react';
 import { useAuth } from '../context/AuthContext';
+import './style/CharacterForm.css';
 
 function CharacterForm({ addCharacter }) {
     const { currentUser, updateCurrentUser } = useAuth();
 
+    // States to hold feats, spells, and items for character submission process
+    const { currentFeatArray, setCurrentFeatArray } = useState([]);
+    const { currentSpellArray, setCurrentSpellArray } = useState([]);
+    const { currentItemArray, setCurrentItemArray } = useState([]);
+
     const [newCharacter, setNewCharacter] = useState({
         name:'',
-        lvl: '',
         class: '',
-        race: ''
+        race: '',
+        lvl: '',
+        HP_max: 0,
+        ascores: [],
+        spells: [],
+        backpack: [],
+        lorewriteup: '',
     });
+
+    // Inputs for character Ascores
+    const inputAscore = {
+        title:'',
+        score:0
+    }
+
+    // Inputs for character Ascores
+    const inputFeat = {
+        title:'',
+        score:0
+    }
+
+    // Inputs for character spells
+    const inputSpell = {
+        title:'',
+        level:0,
+        body:''
+    }
+
+    // Inputs for character items
+    const inputItem = {
+        title:'',
+        weight:0.0,
+        worth:'',
+        body:''
+    }
 
     const handleChange = (e) => {
         setNewCharacter({ ...newCharacter, [e.target.name]: e.target.value});
     };
-
-    //const handleSubmit = (e) => {
-    //    e.preventDefault();
-    //    addCharacter(newCharacter);
-    //    setNewCharacter({name: '', lvl: '', class: '', race: ''});
-    //};
 
 
     const handleSubmit = async (e) => {
@@ -49,7 +81,7 @@ function CharacterForm({ addCharacter }) {
             console.log('Success:', result);
     
             // Optionally, clear the form here if submission was successful
-            setNewCharacter({name: '', lvl: '', class: '', race: ''});
+            setNewCharacter({name: '', class: '', race: '', lvl: '', HP_max: 0, ascores: [], features: [], spells: [], backpack: [], lorewriteup:''});
 
             // Updated currentUser context to grab new added character
             const updatedCharacterList = [...currentUser.characterList, result._id];
@@ -64,6 +96,7 @@ function CharacterForm({ addCharacter }) {
     };
 
     return (
+        <div className ="fullform">
         <form onSubmit={handleSubmit}>
             <input
                 type="text"
@@ -97,8 +130,133 @@ function CharacterForm({ addCharacter }) {
                 placeholder="Race"
                 required
             />
+            <input
+                type="int"
+                name="HP_max"
+                value={newCharacter.HP_max}
+                onChange={handleChange}
+                placeholder="0"
+                required
+            />
+
+            {/** -------------------------- */}
+            {/** Form to handle Ascore inputs. */}
+            <div className = "ascoreform">
+            <form>
+                <p>Ascore Form</p>
+                <input
+                    type="text"
+                    name="ascoreTitle"
+                    value={inputAscore.title}
+                    onChange={handleChange}
+                    placeholder="CON"
+                    required
+                />
+                <input
+                    type="int"
+                    name="ascoreVal"
+                    value={inputAscore.score}
+                    onChange={handleChange}
+                    placeholder="0"
+                    required
+                />
+            </form>
+            </div>
+            {/** Form to handle feat inputs. */}
+            <div className = "featform">
+            <form>
+                <p>Feat Form</p>
+                 <input
+                    type="text"
+                    name="featTitle"
+                    value={inputFeat.title}
+                    onChange={handleChange}
+                    placeholder="Title"
+                    required
+                />
+                <input
+                    type="text"
+                    name="featBody"
+                    value={inputFeat.body}
+                    onChange={handleChange}
+                    placeholder="Body"
+                    required
+                />
+            </form>
+            </div>
+            {/** Form to handle spell inputs. */}
+            <div className = "spellform">
+            <form>
+                <p>Spell Form</p>
+                <input
+                    type="text"
+                    name="spellTitle"
+                    value={inputSpell.title}
+                    onChange={handleChange}
+                    placeholder="Fireball"
+                    required
+                />
+                <input
+                    type="int"
+                    name="spellLevel"
+                    value={inputSpell.level}
+                    onChange={handleChange}
+                    placeholder="1"
+                    required
+                />
+                <input
+                    type="int"
+                    name="spellBody"
+                    value={inputSpell.body}
+                    onChange={handleChange}
+                    placeholder="A fiery ball!"
+                    required
+                />
+            </form>
+            </div>
+            {/** Form to handle item inputs. */}
+            <div className = "itemform">
+            <form>
+                <p>Item Form</p>
+                <input
+                    type="text"
+                    name="itemTitle"
+                    value={inputItem.title}
+                    onChange={handleChange}
+                    placeholder="Stick"
+                    required
+                />
+                <input
+                    type="double"
+                    name="itemWeight"
+                    value={inputItem.weight}
+                    onChange={handleChange}
+                    placeholder="0.0"
+                    required
+                />
+                <input
+                    type="text"
+                    name="itemWorth"
+                    value={inputItem.body}
+                    onChange={handleChange}
+                    placeholder="1 gold"
+                    required
+                />
+            </form>
+            </div>
+            {/** -------------------------- */}
+
+            <input
+                type="text"
+                name="lorewriteup"
+                value={newCharacter.lorewriteup}
+                onChange={handleChange}
+                placeholder="Lore Writeup"
+                required
+            />
             <button type ="submit"> Submit your character! </button>
         </form>
+        </div>
     )
 }
 
